3920f6a3406ddd5fa7e86c741960ca4ca5b3fbaf
Edward <github@edwardcs.com>
2023-02-08 19:53:50 +0200

    Fix BUG: favicon and background images not loading

--- src/functions/change-background.ts
+++ src/functions/change-background.ts
@@ -3,9 +3,9 @@ function changeBodyBackground(icon: string) {
   const body = document.querySelector("body");
 
   if (icon[2] === "d") {
-    body.style.backgroundImage = `url(../src/imgs/background-images/day/${icon}.gif)`;
+    body.style.backgroundImage = `url(./imgs/background-images/day/${icon}.gif)`;
   } else {
-    body.style.backgroundImage = `url(../src/imgs/background-images/night/${icon}.gif)`;
+    body.style.backgroundImage = `url(./imgs/background-images/night/${icon}.gif)`;
   }
 }
 
